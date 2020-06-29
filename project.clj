f317c7f4e20e59d4d8aed13d6d2fc3f58c8c940d
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2020-06-29 16:39:54 +0100

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject albany-statements "0.3.2"
+(defproject albany-statements "0.3.3-SNAPSHOT"
   :description "Albany order forms and statements based on order xls"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
